a1b0a900d7ea59df9e2be2586a8be8a30b8bfc3a
Goulgoul <antoine.gln571@gmail.com>
2022-04-28 10:55:15 +0200

    correctoin

--- src/Ecran.cpp
+++ src/Ecran.cpp
@@ -26,10 +26,25 @@ void Ecran::Initialiser()
     m_Ecran.Init(FULL);
 }
 
+void Ecran::InitialiserPartiellement()
+{
+    m_Paint.Clear(BLANC);
+    m_Paint.SetRotate(ROTATE_90);
+    m_Ecran.Init(PART);
+}
+
 void Ecran::AfficherNom()
 {
     m_Paint.DrawFilledRectangle(0, 114, 250, 128, RACISE);  //A SURCHARGER avec ligne()
     m_Paint.DrawStringAt(0, 112, "LuckyLux", &Font16, BLANC);   //A SURCHARGER avec ligne()
     m_Paint.DrawStringAt(151, 112, "Pre-alpha", &Font16, BLANC);    //A SURCHARGER avec ligne()
     m_Ecran.Display(_IMAGE);
+}
+
+void Ecran::AfficherMode()
+{
+    char* test = new char[20 * 20 / 8];
+    Paint Paint_test(test, 20, 20);  
+    m_Paint(test, EPD_WIDTH, EPD_HEIGHT),
+    
 }
