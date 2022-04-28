c474e31b39d31894ec93857143e750e67c23dc20
Goulgoul <antoine.gln571@gmail.com>
2022-04-28 18:07:28 +0200

    ljk

--- src/main.cpp
+++ src/main.cpp
@@ -90,42 +90,53 @@
 #include "/home/mouss/Documents/Projet-LuckyLux/.pio/libdeps/nano33ble/U8g2_for_Adafruit_GFX-1.8.0/U8g2_for_Adafruit_GFX-1.8.0/src/U8g2_for_Adafruit_GFX.h"
 
 //if you have another microcontroller or another e-ink display module you have to change the following line
-GxEPD2_BW<GxEPD2_290_T94_V2, GxEPD2_290_T94_V2::HEIGHT> display(GxEPD2_290_T94_V2(D10, D9, D8, D7)); // GDEM029T94, Waveshare 2.9" V2 variant
+// GxEPD2_BW<GxEPD2_290_T94_V2, GxEPD2_290_T94_V2::HEIGHT> display(GxEPD2_290_T94_V2(D10, D9, D8, D7)); // GDEM029T94, Waveshare 2.9" V2 variant
+GxEPD2_BW<GxEPD2_213_B72, GxEPD2_213_B72::HEIGHT> display(GxEPD2_213_B72(D10, D9, D8, D7)); // GDEM029T94, Waveshare 2.9" V2 variant
 
 U8G2_FOR_ADAFRUIT_GFX u8g2Fonts;
 
+// void setup()
+// {
+//   display.init();
+//   display.setTextColor(NOIR);
+//   display.firstPage();
+//   display.setRotation(1);
+
+//   u8g2Fonts.begin(display); // connect u8g2 procedures to Adafruit GFX
+
+
+//   uint16_t bg = BLANC;
+//   uint16_t fg = NOIR;
+//   u8g2Fonts.setForegroundColor(fg);         // apply Adafruit GFX color
+//   u8g2Fonts.setBackgroundColor(bg);
+
+//     display.fillScreen(BLANC);
+
+//     u8g2Fonts.setFont(u8g2_font_fub14_tr);   //font is set
+//     u8g2Fonts.setCursor(10, 10);             //cursor(x,y)
+//     u8g2Fonts.print("Millis: ");   //print the text
+ 
+// }
+
+
+// void loop() {
+//   display.setPartialWindow(0, 0, 250, 128);
+//   display.firstPage();
+
+//   do{
+//     display.fillScreen(BLANC);
+//     u8g2Fonts.setCursor(0, 0);
+//     u8g2Fonts.print(millis());
+//   }while(display.nextPage());
+// }
 void setup()
 {
   display.init();
-  display.setTextColor(NOIR);
-  display.firstPage();
-  display.setRotation(1);
-
-  u8g2Fonts.begin(display); // connect u8g2 procedures to Adafruit GFX
-  delay(1000);
-
-  uint16_t bg = BLANC;
-  uint16_t fg = NOIR;
-  u8g2Fonts.setForegroundColor(fg);         // apply Adafruit GFX color
-  u8g2Fonts.setBackgroundColor(bg);
-
-  do{
-    display.fillScreen(BLANC);
-
-    u8g2Fonts.setFont(u8g2_font_fub14_tr);   //font is set
-    u8g2Fonts.setCursor(0, 0);             //cursor(x,y)
-    u8g2Fonts.print("Millis: ");   //print the text
-  }while (display.nextPage());
+  display.setFullWindow();
+  display.clearScreen();
 }
 
+void loop()
+{
 
-void loop() {
-  display.setPartialWindow(0, 0, 250, 128);
-  display.firstPage();
-
-  do{
-    display.fillScreen(BLANC);
-    u8g2Fonts.setCursor(0, 0);
-    u8g2Fonts.print(millis());
-  }while(display.nextPage());
 }
