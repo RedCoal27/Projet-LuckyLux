f2fe850f30870910433779eff4e7c075c1744d68
RedCoal <redcoal.pro@gmail.com>
2022-04-28 10:39:41 +0200

    ecran.cpp

--- src/Ecran.cpp
+++ src/Ecran.cpp
@@ -1,13 +1,12 @@
 #include "Ecran.h"
-unsigned char _bgIMAGE[4000];
-unsigned char _fgIMAGE[4000];
+unsigned char _IMAGE[4000];
 
 Ecran::Ecran():
-    m_fgPaint(_fgIMAGE, EPD_WIDTH, EPD_HEIGHT),
-    m_bgPaint(_bgIMAGE, EPD_WIDTH, EPD_HEIGHT),
+    m_Paint(_IMAGE, EPD_WIDTH, EPD_HEIGHT),
     m_Ecran()
 {
-
+    // m_Ecran.Init(FULL);
+    // m_Ecran.Display(_IMAGE);
 }
 
 Ecran::~Ecran()
@@ -16,32 +15,21 @@ Ecran::~Ecran()
 
 void Ecran::Effacer()
 {
-    m_Ecran.Clear();
+    m_Paint.Clear(BLANC);
+    
 }
 
 void Ecran::Initialiser()
 {
+    m_Paint.Clear(BLANC);
+    m_Paint.SetRotate(ROTATE_90);
     m_Ecran.Init(FULL);
-    m_Ecran.Clear();
-
-    m_fgPaint.SetRotate(ROTATE_90);
-    m_bgPaint.Clear(BLANC);
-    m_bgPaint.SetRotate(ROTATE_90);
-    m_fgPaint.Clear(BLANC);
-    
-}
+}//
 
 void Ecran::AfficherNom()
 {
-    m_bgPaint.DrawFilledRectangle(0, 113, 250, 128, RACISE);  //A SURCHARGER avec ligne()
-    m_bgPaint.DrawStringAt(0, 112, "LuckyLux", &Font16, BLANC);   //A SURCHARGER avec ligne()
-    m_bgPaint.DrawStringAt(151, 112, "Pre-alpha", &Font16, BLANC);    //A SURCHARGER avec ligne()
-    m_Ecran.DisplayPartBaseImage(_bgIMAGE);
-}
-
-void Ecran::AfficherMode(const char* strMode)
-{
-    m_Ecran.Init(PART);
-    m_fgPaint.DrawStringAt(10, 60,  strMode, &Font16, RACISE);
-    m_Ecran.DisplayPart(_fgIMAGE);
+    m_Paint.DrawFilledRectangle(0, 114, 250, 128, RACISE);  //A SURCHARGER avec ligne()
+    m_Paint.DrawStringAt(0, 112, "LuckyLux", &Font16, BLANC);   //A SURCHARGER avec ligne()
+    m_Paint.DrawStringAt(151, 112, "Pre-alpha", &Font16, BLANC);    //A SURCHARGER avec ligne()
+    m_Ecran.Display(_IMAGE);
 }
